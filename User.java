24241beebbdf619bcb6cb4f1f799381712261f3c
daudroi <achmaddaud42@gmail.com>
2025-09-24 21:47:30 +0700

    class user

---
+++ User.java
@@ -0,0 +1,15 @@
+public class User {
+    private String nama;
+    private String email;
+    private String password;
+
+    public User(String nama, String email, String password) {
+        this.nama = nama;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getNama() { return nama; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+}
