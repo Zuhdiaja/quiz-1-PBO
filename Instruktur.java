bf6f93abdc2df4fd6d705387ca1bd28999fe12b3
daudroi <achmaddaud42@gmail.com>
2025-09-25 16:44:50 +0700

    instruktur

--- Instruktur.java
+++ Instruktur.java
@@ -1,3 +1,20 @@
-public class Instruktur extends Pengguna {
+public class Instruktur extends User {
+    private String bidangKeahlian;
+
+    public Instruktur(String nama, String email, String password, String bidangKeahlian) {
+        super(nama, email, password);
+        this.bidangKeahlian = bidangKeahlian;
+    }
+
+    public String getBidangKeahlian() { return bidangKeahlian; }
+
     
+     public void buatKursus(String namaKursus, double harga) {
+        System.out.println("Instruktur " + getNama() + " membuat kursus: " 
+            + namaKursus + " (Rp" + harga + ")");
+    }
+
+    public void unggahKonten(String namaKonten) {
+        System.out.println("Instruktur " + getNama() + " mengunggah konten: " + namaKonten);
+    }
 }
