e48d1a3a1c30e69ff3885a3b6fad28e6cd42bace
daudroi <achmaddaud42@gmail.com>
2025-09-25 16:43:51 +0700

    kasih harga kursus

--- Kursus.java
+++ Kursus.java
@@ -2,11 +2,13 @@ public class Kursus {
     private int id;
     private String judul;
     private String deskripsi;
+    private double harga;
 
     public  Kursus(int id, String judul, String deskripsi){
         this.id = id;
         this.judul = judul;
         this.deskripsi = deskripsi;
+        this.harga = harga;
     }
 
     public int getId(){
@@ -31,6 +33,8 @@ public class Kursus {
     public void setDeskripsi(String deskripsi){
         this.deskripsi = deskripsi;
     }
+    public double getHarga() { return harga; }
+    public void setHarga(double harga) { this.harga = harga; }
 
     // public void tambahKonten(Konten konten){
     //     System.out.println("Konten '" + konten.getJudul() + "' ditambahkan ke kursus " + judul);
